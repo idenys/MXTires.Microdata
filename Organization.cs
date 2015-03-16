c40201a38e69e2002aeb5316228655f43541b282
Ivan Denyssevych <idenys@yahoo.com>
2015-03-16 01:32:17 -0700

    Added properties to Organization

--- Organization.cs
+++ Organization.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.Collections.Generic;
 using Newtonsoft.Json;
 namespace MXTires.Microdata
 {
@@ -8,12 +9,36 @@ namespace MXTires.Microdata
         /// PostalAddress 	Physical address of the item.
         /// </summary>
         [JsonProperty("address")]
-        public PostalAddress Address {get;set;}
+        public PostalAddress Address { get; set; }
 
         /// <summary>
         /// The location of the event, organization or action.
         /// </summary>
         [JsonProperty("location")]
-        public Place Location { get; set; }	
+        public Place Location { get; set; }
+
+        /// <summary>
+        /// The telephone number.
+        /// </summary>
+        [JsonProperty("telephone")]
+        public string Telephone { get; set; }
+
+        /// <summary>
+        /// A review of the item. Supersedes reviews.
+        /// </summary>
+        [JsonProperty("review")]
+        public Review Review { get; set; }
+
+        /// <summary>
+        /// Offer - A pointer to products or services offered by the organization or person.
+        /// </summary>
+        [JsonProperty("makesOffer")]
+        public List<Offer> MakesOffer { get; set; }
+
+        /// <summary>
+        /// Text - The official name of the organization, e.g. the registered company name.
+        /// </summary>
+        [JsonProperty("legalName")]
+        public string LegalName { get; set; }
     }
 }
