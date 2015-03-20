a7b0fc76b7ff765cd31493eb355160ac30effc69
Ivan Denyssevych <idenys@yahoo.com>
2015-03-20 00:12:03 -0700

    Organization

--- Organization.cs
+++ Organization.cs
@@ -40,5 +40,29 @@ namespace MXTires.Microdata
         /// </summary>
         [JsonProperty("legalName")]
         public string LegalName { get; set; }
+
+        /// <summary>
+        /// AggregateRating - The overall rating, based on a collection of reviews or ratings, of the item.
+        /// </summary>
+        [JsonProperty("aggregateRating")]
+        public AggregateRating AggregateRating { get; set; }
+
+        /// <summary>
+        /// Brand  or Organization 	The brand(s) associated with a product or service, or the brand(s) maintained by an organization or business person.
+        /// </summary>
+        [JsonProperty("brand")]
+        public List<Thing> Brand { get; set; }
+
+        /// <summary>
+        /// URL - An associated logo.
+        /// </summary>
+        [JsonProperty("logo")]
+        public string Logo { get; set; }
+
+       /// <summary>
+        /// ContactPoint - A contact point for a person or organization. Supersedes contactPoints.
+        /// </summary>
+        [JsonProperty("contactPoint")]
+        public ContactPoint ContactPoint { get; set; }
     }
 }
