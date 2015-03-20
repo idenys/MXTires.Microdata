fc50b4a034b9b57b6b8420441eaed3771271a1da
Ivan Denyssevych <idenys@yahoo.com>
2015-03-20 09:13:07 -0700

    more on CreativeWork

--- Corporation.cs
+++ Corporation.cs
@@ -6,8 +6,15 @@ using Newtonsoft.Json;
 
 namespace MXTires.Microdata
 {
-    public class Corporation:Organization
+    /// <summary>
+    /// Organization: A business corporation.
+    /// </summary>
+    public class Corporation : Organization
     {
+        /// <summary>
+        /// The exchange traded instrument associated with a Corporation object. The tickerSymbol is expressed as an exchange and an instrument name separated by a space character. 
+        /// For the exchange component of the tickerSymbol attribute, we reccommend using the controlled vocaulary of Market Identifier Codes (MIC) specified in ISO15022.
+        /// </summary>
         [JsonProperty("tickerSymbol")]
         public string TickerSymbol { get; set; }
     }
--- CreativeWork.cs
+++ CreativeWork.cs
@@ -6,6 +6,9 @@ using Newtonsoft.Json;
 
 namespace MXTires.Microdata
 {
+    /// <summary>
+    /// The most generic kind of creative work, including books, movies, photographs, software programs, etc.
+    /// </summary>
     public class CreativeWork : Thing
     {
         /// <summary>
@@ -17,65 +20,83 @@ namespace MXTires.Microdata
         /// <summary>
         /// Indicates that the resource is compatible with the referenced accessibility API (WebSchemas wiki lists possible values).
         /// </summary>
-        public string accessibilityAPI { get; set; }
+        [JsonProperty("accessibilityAPI")]
+        public string AccessibilityAPI { get; set; }
         /// <summary>
         /// Text 	Identifies input methods that are sufficient to fully control the described resource (WebSchemas wiki lists possible values).
         /// </summary>
-        public string accessibilityControl { get; set; }
+        [JsonProperty("accessibilityControl")]
+        public string AccessibilityControl { get; set; }
 
         /// <summary>
         /// Text 	Content features of the resource, such as accessible media, alternatives and supported enhancements for 
         /// accessibility (WebSchemas wiki lists possible values).
         /// </summary>
-        public string accessibilityFeature { get; set; }
+        [JsonProperty("accessibilityFeature")]
+        public string AccessibilityFeature { get; set; }
 
         /// <summary>
         /// A characteristic of the described resource that is physiologically dangerous to some users. 
         /// Related to WCAG 2.0 guideline 2.3. (WebSchemas wiki lists possible values)
         /// </summary>
-        public string accessibilityHazard { get; set; }
+        [JsonProperty("accessibilityHazard")]
+        public string AccessibilityHazard { get; set; }
+
         /// <summary>
         /// Person 	Specifies the Person that is legally accountable for the CreativeWork.
         /// </summary>
-        public Person accountablePerson { get; set; }
+        [JsonProperty("accountablePerson")]
+        public Person AccountablePerson { get; set; }
 
         /// <summary>
         /// AggregateRating 	The overall rating, based on a collection of reviews or ratings, of the item.
         /// </summary>
-        public AggregateRating aggregateRating { get; set; }
+        [JsonProperty("aggregateRating")]
+        public AggregateRating AggregateRating { get; set; }
+
         /// <summary>
         /// Text 	A secondary title of the CreativeWork.
         /// </summary>
-        public string alternativeHeadline { get; set; }
+        [JsonProperty("alternativeHeadline")]
+        public string AlternativeHeadline { get; set; }
 
         /// <summary>
         /// MediaObject 	A media object that encodes this CreativeWork. This property is a synonym for encoding.
         /// </summary>
-        public MediaObject associatedMedia { get; set; }
+        [JsonProperty("associatedMedia")]
+        public MediaObject AssociatedMedia { get; set; }
+
         /// <summary>
         /// Audience 	The intended audience of the item, i.e. the group for whom the item was created.
         /// </summary>
-        public Audience audience { get; set; }
+        [JsonProperty("audience")]
+        public Audience Audience { get; set; }
 
         /// <summary>
         /// An embedded audio object.
         /// </summary>
-        public AudioObject audio { get; set; }
+        [JsonProperty("audio")]
+        public AudioObject Audio { get; set; }
+
         /// <summary>
-        /// Person  or 
-        /// Organization 	The author of this content. 
+        /// Person  or  Organization - The author of this content. 
         /// Please note that author is special in that HTML 5 provides a special mechanism for indicating authorship via the rel tag. 
         /// That is equivalent to this and may be used interchangeably.
         /// </summary>
-        public Author author { get; set; }
+        [JsonProperty("author")]
+        public Author Author { get; set; }
+
         /// <summary>
         /// Text 	An award won by this person or for this creative work. Supersedes awards.
         /// </summary>
-        public string award { get; set; }
+        [JsonProperty("award")]
+        public string Award { get; set; }
+
         /// <summary>
         /// Person 	Fictional person connected with a creative work.
         /// </summary>
-        public Person character { get; set; }
+        [JsonProperty("character")]
+        public Person Character { get; set; }
 
         /// <summary>
         /// 	CreativeWork  {get;set;} or 
@@ -83,27 +104,37 @@ namespace MXTires.Microdata
         /// </summary>
         [JsonProperty("citation")]
         public CreativeWork Citation { get; set; }
+
         /// <summary>
         /// UserComments  or Comment - Comments, typically from users, on this CreativeWork.
         /// </summary>
-        public Event comment { get; set; }
+        [JsonProperty("comment")]
+        public object Comment { get; set; }
 
         /// <summary>
         /// Integer 	The number of comments this CreativeWork (e.g. Article, Question or Answer) has received. This is most applicable to works published in Web sites with commenting system; additional comments may exist elsewhere.
         /// </summary>
-        public Int32? commentCount { get; set; }
+        [JsonProperty("commentCount")]
+        public Int32? CommentCount { get; set; }
+
         /// <summary>
         /// Place - The location of the content.
         /// </summary>
-        public Place contentLocation { get; set; }
+        [JsonProperty("contentLocation")]
+        public Place ContentLocation { get; set; }
+        
         /// <summary>
         /// Text 	Official rating of a piece of content—for example,'MPAA PG-13'.
         /// </summary>
-        public string contentRating { get; set; }
+        [JsonProperty("contentRating")]
+        public string ContentRating { get; set; }
+        
         /// <summary>
         /// Person  or Organization - A secondary contributor to the CreativeWork.
         /// </summary>
-        public Thing contributor { get; set; }
+        [JsonProperty("contributor")]
+        public Thing Contributor { get; set; }
+
         /// <summary>
         /// Person  or Organization - The party holding the legal copyright to the CreativeWork.
         /// </summary>
@@ -119,92 +150,117 @@ namespace MXTires.Microdata
         /// <summary>
         /// Person  or Organization - The creator/author of this CreativeWork or UserComments. This is the same as the Author property for CreativeWork.
         /// </summary>
-        public Thing creator { get; set; }
+        [JsonProperty("creator")]
+        public Thing Creator { get; set; }
 
         /// <summary>
         /// Date 	The date on which the CreativeWork was created.
         /// </summary>
-        public DateTime? dateCreated { get; set; }
+        [JsonProperty("dateCreated")]
+        public DateTime? DateCreated { get; set; }
+
         /// <summary>
         /// Date 	The date on which the CreativeWork was most recently modified.
         /// </summary>
-        public DateTime? dateModified { get; set; }
+        [JsonProperty("dateModified")]
+        public DateTime? DateModified { get; set; }
+
         /// <summary>
         /// Date 	Date of first broadcast/publication.
         /// </summary>
-        public DateTime? datePublished { get; set; }
+        [JsonProperty("datePublished")]
+        public DateTime? DatePublished { get; set; }
 
         /// <summary>
         ///  URL 	A link to the page containing the comments of the CreativeWork.
         /// </summary>
-        public URL discussionUrl { get; set; }
+        [JsonProperty("discussionUrl")]
+        public string DiscussionUrl { get; set; }
 
         /// <summary>
         /// Person 	Specifies the Person who edited the CreativeWork.
         /// </summary>
-        public Person editor { get; set; }
+        [JsonProperty("editor")]
+        public Person Editor { get; set; }
 
         /// <summary>
         /// AlignmentObject 	An alignment to an established educational framework.
         /// </summary>
-        public AlignmentObject educationalAlignment { get; set; }
+        [JsonProperty("educationalAlignment")]
+        public AlignmentObject EducationalAlignment { get; set; }
+
         /// <summary>
         /// Text 	The purpose of a work in the context of education; for example, 'assignment', 'group work'.
         /// </summary>
-        public string educationalUse { get; set; }
+        [JsonProperty("educationalUse")]
+        public string EducationalUse { get; set; }
+
         /// <summary>
         /// MediaObject 	A media object that encodes this CreativeWork. This property is a synonym for associatedMedia. Supersedes encodings.
         /// </summary>
-        public MediaObject encoding { get; set; }
+        [JsonProperty("encoding")]
+        public MediaObject Encoding { get; set; }
 
         /// <summary>
         /// CreativeWork - A creative work that this work is an example/instance/realization/derivation of. Inverse property: workExample.
         /// </summary>
-        public CreativeWork exampleOfWork { get; set; }
+        [JsonProperty("exampleOfWork")]
+        public CreativeWork ExampleOfWork { get; set; }
+
         /// <summary>
         /// Text - Genre of the creative work or group.
         /// </summary>
-        public string genre { get; set; }
+        [JsonProperty("genre")]
+        public string Genre { get; set; }
 
         /// <summary>
         /// CreativeWork - Indicates a CreativeWork that is (in some sense) a part of this CreativeWork. Inverse property: isPartOf.
         /// </summary>
-        public CreativeWork hasPart { get; set; }
+        [JsonProperty("hasPart")]
+        public CreativeWork HasPart { get; set; }
 
         /// <summary>
         /// Text -	Headline of the article
         /// </summary>
-        public string headline { get; set; }
+        [JsonProperty("headline")]
+        public string Headline { get; set; }
 
         /// <summary>
         /// Text - The language of the content. please use one of the language codes from the IETF BCP 47 standard.
         /// </summary>
-        public string inLanguage { get; set; }
+        [JsonProperty("inLanguage")]
+        public string InLanguage { get; set; }
 
         /// <summary>
         /// Text 	A count of a specific user interactions with this item—for example, 20 UserLikes, 5 UserComments, or 300 UserDownloads. The user interaction type should be one of the sub types of UserInteraction.
         /// </summary>
-        public string interactionCount { get; set; }
+        [JsonProperty("interactionCount")]
+        public string InteractionCount { get; set; }
 
         /// <summary>
         /// Text - The predominant mode of learning supported by the learning resource. Acceptable values are 'active', 'expositive', or 'mixed'.
         /// </summary>
-        public string interactivityType { get; set; }
+        [JsonProperty("interactivityType")]
+        public string InteractivityType { get; set; }
 
         /// <summary>
         /// URL 	A resource that was used in the creation of this resource. This term can be repeated for multiple sources. For example, http://example.com/great-multiplication-intro.html
         /// </summary>
-        public URL isBasedOnUrl { get; set; }
+        [JsonProperty("isBasedOnUrl")]
+        public string IsBasedOnUrl { get; set; }
 
         /// <summary>
         /// Boolean 	Indicates whether this content is family friendly.
         /// </summary>
-        public bool? isFamilyFriendly { get; set; }
+        [JsonProperty("isFamilyFriendly")]
+        public bool? IsFamilyFriendly { get; set; }
+        
         /// <summary>
         /// CreativeWork 	Indicates a CreativeWork that this CreativeWork is (in some sense) part of.
         /// Inverse property: hasPart.
         /// </summary>
-        public CreativeWork isPartOf { get; set; }
+        [JsonProperty("isPartOf")]
+        public CreativeWork IsPartOf { get; set; }
 
         /// <summary>
         /// Text -	Keywords or tags used to describe this content. Multiple entries in a keywords list are typically delimited by commas.
@@ -215,100 +271,126 @@ namespace MXTires.Microdata
         /// <summary>
         /// Text - The predominant type or kind characterizing the learning resource. For example, 'presentation', 'handout'.
         /// </summary>
-        public string learningResourceType { get; set; }
+        [JsonProperty("learningResourceType")]
+        public string LearningResourceType { get; set; }
 
         /// <summary>
         /// URL  or CreativeWork - A license document that applies to this content, typically indicated by URL.
         /// </summary>
-        public Thing license { get; set; }
+        [JsonProperty("license")]
+        public Thing License { get; set; }
+
         /// <summary>
         /// Thing 	Indicates that the CreativeWork contains a reference to, but is not necessarily about a concept.
         /// </summary>
-        public Thing mentions { get; set; }
+        [JsonProperty("mentions")]
+        public Thing Mentions { get; set; }
+
         /// <summary>
         /// Offer 	An offer to provide this item—for example, an offer to sell a product, rent the DVD of a movie, or give away tickets to an event.
         /// </summary>
-        public Offer offers { get; set; }
+        [JsonProperty("offers")]
+        public IList<Offer> Offers { get; set; }
+
         /// <summary>
         /// Integer  or Text - The position of an item in a series or sequence of items.
         /// </summary>
-        public Int32? position { get; set; }
+        [JsonProperty("position")]
+        public Int32? Position { get; set; }
 
         /// <summary>
         /// Person  or  Organization  -	The service provider, service operator, or service performer; 
         /// the goods producer. Another party (a seller) may offer those services or goods on behalf of the provider. 
         /// A provider may also serve as the seller. Supersedes carrier.
         /// </summary>
-        public Thing provider { get; set; }
+        [JsonProperty("provider")]
+        public Thing Provider { get; set; }
 
         /// <summary>
         /// Organization - The publisher of the creative work.
         /// </summary>
-        public Organization publisher { get; set; }
+        [JsonProperty("publisher")]
+        public Organization Publisher { get; set; }
+
         /// <summary>
         /// URL - Link to page describing the editorial principles of the organization primarily responsible for the creation of the CreativeWork.
         /// </summary>
-        public URL publishingPrinciples { get; set; }
+        [JsonProperty("publishingPrinciples")]
+        public string PublishingPrinciples { get; set; }
 
         /// <summary>
         /// Event -	The Event where the CreativeWork was recorded. The CreativeWork may capture all or part of the event.
         /// Inverse property: recordedIn.
         /// </summary>
-        public Event recordedAt { get; set; }
+        [JsonProperty("recordedAt")]
+        public Event RecordedAt { get; set; }
 
         /// <summary>
         /// PublicationEvent 	The place and time the release was issued, expressed as a PublicationEvent.
         /// </summary>
-        public PublicationEvent releasedEvent { get; set; }
+        [JsonProperty("releasedEvent")]
+        public PublicationEvent ReleasedEvent { get; set; }
 
         /// <summary>
         /// Review 	A review of the item. Supersedes reviews.
         /// </summary>
-        public Review review { get; set; }
+        [JsonProperty("review")]
+        public Review Review { get; set; }
+
         /// <summary>
         /// Organization - The Organization on whose behalf the creator was working.
         /// </summary>
-        public Organization sourceOrganization { get; set; }
+        [JsonProperty("sourceOrganization")]
+        public Organization SourceOrganization { get; set; }
+
         /// <summary>
         /// Text -The textual content of this CreativeWork.
         /// </summary>
-        public string text { get; set; }
+        [JsonProperty("text")]
+        public string Text { get; set; }
 
         /// <summary>
         /// URL - A thumbnail image relevant to the Thing.
         /// </summary>
-        public URL thumbnailUrl { get; set; }
+        [JsonProperty("thumbnailUrl")]
+        public string ThumbnailUrl { get; set; }
 
         /// <summary>
         /// Duration 	Approximate or typical time it takes to work with or through this learning resource for the typical intended target audience, e.g. 'P30M', 'P1H25M'.
         /// </summary>
-        public Duration timeRequired { get; set; }
+        [JsonProperty("timeRequired")]
+        public Duration TimeRequired { get; set; }
 
         /// <summary>
         /// Person  or Organization - Organization or person who adapts a creative work to different languages, regional differences and technical requirements of a target market.
         /// </summary>
-        public Thing translator { get; set; }
+        [JsonProperty("translator")]
+        public Thing Translator { get; set; }
 
         /// <summary>
         /// Text 	The typical expected age range, e.g. '7-9', '11-'.
         /// </summary>
-        public string typicalAgeRange { get; set; }
+        [JsonProperty("typicalAgeRange")]
+        public string TypicalAgeRange { get; set; }
 
         /// <summary>
         /// Number - The version of the CreativeWork embodied by a specified resource.
         /// </summary>
-        public Int32? version { get; set; }
+        [JsonProperty("version")]
+        public Int32? Version { get; set; }
 
         /// <summary>
         /// VideoObject 	An embedded video object.
         /// </summary>
-        public VideoObject video { get; set; }
+        [JsonProperty("video")]
+        public VideoObject Video { get; set; }
 
         /// <summary>
         /// CreativeWork 	Example/instance/realization/derivation of the concept of this creative work. eg. 
         /// The paperback edition, first edition, or eBook. 
         /// Inverse  property: exampleOfWork.
         /// </summary>
-        public CreativeWork workExample { get; set; }
+        [JsonProperty("workExample")]
+        public CreativeWork WorkExample { get; set; }
     }
 }
