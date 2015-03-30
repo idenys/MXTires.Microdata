4258ea519c44c581c8496b97c2b4222e0197cecd
Ivan Denyssevych <idenys@yahoo.com>
2015-03-29 23:32:42 -0700

    Tire and Wheel

---
+++ Tire.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace MXTires.Microdata
+{
+    public class Tire : Product
+    {
+        [JsonProperty("@type", Order = 2)]
+        public new string Type { get { return "Product/Tire"; } }
+
+        //[JsonProperty("additionalType")]
+        //public new string AdditionalType { get { return "http://www.1010tires.com/About/Tire-Tech"; } }
+
+        //public Tire()
+        //{
+        //    base.SameAs = new List<string>();
+        //    SameAs.Add("http://en.wikipedia.org/wiki/Tire");
+        //}
+    }
+}
---
+++ Wheel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace MXTires.Microdata
+{
+    public class Wheel : Product
+    {
+        [JsonProperty("@type", Order = 2)]
+        public new string Type { get { return "Product/Wheel"; } }
+
+    //    [JsonProperty("additionalType")]
+    //    public new string AdditionalType { get { return "http://www.1010tires.com/About/Wheel-Tech"; } }
+
+    //    public Wheel() {
+    //        base.SameAs = new List<string>();
+    //        SameAs.Add("http://en.wikipedia.org/wiki/Wheel");
+    //    }
+    //}
+}
