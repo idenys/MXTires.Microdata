aa42ef11798901979e2dcd1b3246de33ce85a8e2
Ivan Denyssevych <idenys@yahoo.com>
2015-04-07 07:01:09 -0700

    start property

---
+++ Intangible/Property.cs
@@ -0,0 +1,38 @@
+﻿#region License
+// Copyright (c) 2015 1010Tires.com
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+#endregion
+
+namespace MXTires.Microdata.Intangible
+{
+    /// <summary>
+    /// A property, used to indicate attributes and relationships of some Thing; equivalent to rdf:Property.
+    /// </summary>
+    public class Property : Thing
+    {
+//        domainIncludes	Class 	Relates a property to a class that is (one of) the type(s) the property is expected to be used on.
+//inverseOf	Property 	Relates a property to a property that is its inverse. Inverse properties relate the same pairs of items to each other, but in reversed direction. For example, the 'alumni' and 'alumniOf' properties are inverseOf each other. Some properties don't have explicit inverses; in these situations RDFa and JSON-LD syntax for reverse properties can be used.
+//rangeIncludes	Class 	Relates a property to a class that constitutes (one of) the expected type(s) for values of the property.
+//supersededBy	Property 	Relates a property to one that supersedes it.
+    }
+}
