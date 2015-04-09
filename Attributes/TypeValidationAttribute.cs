214523f471465dff3c1bdfbdc6bddca412f8931b
Ivan Denyssevych <idenys@yahoo.com>
2015-04-09 15:44:03 -0700

    validation in progress

--- Attributes/TypeValidationAttribute.cs
+++ Attributes/TypeValidationAttribute.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MXTires.Microdata.Validators;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 
 namespace MXTires.Microdata.Attributes
 {
     [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
-    public class TypeValidationAttribute : Attribute
+    public class TypeValidationAttribute : ValidationAttribute
     {
         public Type AcceptedType1 { get; set; }
         public Type AcceptedType2 { get; set; }
@@ -25,20 +27,34 @@ namespace MXTires.Microdata.Attributes
         //        throw new ArgumentException("Validator Attribute param not validator");
         //    }
         //    type = propertyClassType;
-        //public bool IsValid(Type )
 
-        public virtual Validator ValidatorInstance
+        public override bool IsValid(object value)
         {
-            get
-            {
-                if (AcceptedTypes != null) return new TypeValidator(AcceptedTypes);
-                return new TypeValidator(AcceptedType1, AcceptedType2);
-            }
+            if (AcceptedTypes == null) AcceptedTypes = new List<Type>();
+            if (AcceptedType1 != null) AcceptedTypes.Add(AcceptedType1);
+            if (AcceptedType2 != null) AcceptedTypes.Add(AcceptedType2);
+            var validator = new TypeValidator(AcceptedTypes);
+            return validator.IsValid(value.GetType());
         }
 
-        public Type ValidatorType
+        //public virtual Validator ValidatorInstance
+        //{
+        //    get
+        //    {
+        //        if (AcceptedTypes != null) return new TypeValidator(AcceptedTypes);
+        //        return new TypeValidator(AcceptedType1, AcceptedType2);
+        //    }
+        //}
+
+        //public Type ValidatorType
+        //{
+        //    get { return typeof(TypeValidator); }
+        //}
+
+        public override string FormatErrorMessage(string name)
         {
-            get { return typeof(TypeValidator); }
+            return String.Format(CultureInfo.CurrentCulture,
+              ErrorMessageString, name, AcceptedTypes.ToArray());
         }
     }
 }
