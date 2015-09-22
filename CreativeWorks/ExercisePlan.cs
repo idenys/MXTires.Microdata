9b1a57131d4255b7a587298a3a781bda42abe392
Ivan Denyssevych <idenys@yahoo.com>
2015-09-22 07:29:56 -0700

    ExercisePlan

--- CreativeWorks/ExercisePlan.cs
+++ CreativeWorks/ExercisePlan.cs
@@ -20,15 +20,46 @@ namespace MXTires.Microdata.CreativeWorks
         [JsonProperty("activityDuration")]
         public Duration ActivityDuration { get; set; }
 
-       // http://schema.org/ExercisePlan
+        /// <summary>
+        /// Text 	How often one should engage in the activity.
+        /// </summary>
+        [JsonProperty("activityFrequency")]
+        public string ActivityFrequency { get; set; }
 
-//        activityDuration	Duration 	Length of time to engage in the activity.
-//activityFrequency	Text 	How often one should engage in the activity.
-//additionalVariable	Text 	Any additional component of the exercise prescription that may need to be articulated to the patient. This may include the order of exercises, the number of repetitions of movement, quantitative distance, progressions over time, etc.
-//exerciseType	Text 	Type(s) of exercise or activity, such as strength training, flexibility training, aerobics, cardiac rehabilitation, etc.
-//intensity	Text 	Quantitative measure gauging the degree of force involved in the exercise, for example, heartbeats per minute. May include the velocity of the movement.
-//repetitions	Number 	Number of times one should repeat the activity.
-//restPeriods	Text 	How often one should break from the activity.
-//workload	Energy 	Quantitative measure of the physiologic output of the exercise; also referred to as energy expenditure.
+        /// <summary>
+        /// Text - Any additional component of the exercise prescription that may need to be articulated to the patient. This may include the order of exercises, the number of repetitions of movement, quantitative distance, progressions over time, etc.
+        /// </summary>
+        [JsonProperty("additionalVariable")]
+        public string AdditionalVariable { get; set; }
+
+        /// <summary>
+        /// Text 	Type(s) of exercise or activity, such as strength training, flexibility training, aerobics, cardiac rehabilitation, etc.
+        /// </summary>
+        [JsonProperty("exerciseType")]
+        public string ExerciseType { get; set; }
+
+        /// <summary>
+        /// Text 	Quantitative measure gauging the degree of force involved in the exercise, for example, heartbeats per minute. May include the velocity of the movement.
+        /// </summary>
+        [JsonProperty("intensity")]
+        public string Intensity { get; set; }
+
+        /// <summary>
+        /// Number 	Number of times one should repeat the activity.
+        /// </summary>
+        [JsonProperty("repetitions")]
+        public Int32? Repetitions { get; set; }
+
+        /// <summary>
+        /// Text 	How often one should break from the activity.
+        /// </summary>
+        [JsonProperty("restPeriods")]
+        public string RestPeriods { get; set; }
+
+        /// <summary>
+        /// Energy 	Quantitative measure of the physiologic output of the exercise; also referred to as energy expenditure.
+        /// </summary>
+        //[JsonProperty("workload")]
+        //public Energy workload	{ get; set; } 	
     }
 }
