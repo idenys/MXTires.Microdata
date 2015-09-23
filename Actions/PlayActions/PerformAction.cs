252d6d77692fb996882b4f1f5580fdc83a72a799
Ivan Denyssevych <idenys@yahoo.com>
2015-09-22 23:19:29 -0700

    PerformAction

---
+++ Actions/PlayActions/PerformAction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MXTires.Microdata.LocalBusinesses;
+using Newtonsoft.Json;
+
+namespace MXTires.Microdata.Actions.PlayActions
+{
+    /// <summary>
+    /// The act of posing a question / favor to someone.
+    /// Related actions:
+    /// ReplyAction: Appears generally as a response to MXTires
+    /// </summary>
+    public class PerformAction : PlayAction
+    {
+        /// <summary>
+        /// EntertainmentBusiness 	A sub property of location. The entertainment business where the action occurred.
+        /// </summary>
+        [JsonProperty("entertainmentBusiness")]
+        public EntertainmentBusiness EntertainmentBusiness { get; set; }
+    }
+}
---
+++ LocalBusinesses/EntertainmentBusiness .cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace MXTires.Microdata.LocalBusinesses
+{
+    /// <summary>
+    /// A business providing entertainment.
+    /// </summary>
+    public class EntertainmentBusiness : LocalBusiness
+    {
+    }
+}
