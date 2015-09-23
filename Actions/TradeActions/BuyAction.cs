09e3ed700f4ec6fcfcd3680359cccecb172bf2ef
Ivan Denyssevych <idenys@yahoo.com>
2015-09-22 23:29:55 -0700

    TradeActions

---
+++ Actions/TradeActions/BuyAction.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MXTires.Microdata.Validators;
+using Newtonsoft.Json;
+
+namespace MXTires.Microdata.Actions.TradeActions
+{
+    /// <summary>
+    /// The act of giving money to a seller in exchange for goods or services rendered. An agent buys an object, product, or service from a seller for a price. Reciprocal of SellAction.
+    /// </summary>
+    public class BuyAction : TradeAction
+    {
+        object seller;
+
+        /// <summary>
+        /// Organization  or Person - An entity which offers (sells / leases / lends / loans) the services / goods. A seller may also be a provider. Supersedes merchant, vendor.
+        /// </summary>
+        [JsonProperty("seller")]
+        public object Seller
+        {
+            get { return seller; }
+            set
+            {
+                var validator = new TypeValidator(typeof(Organization), typeof(Person));
+                validator.Validate(value);
+                seller = value;
+            }
+        }
+    }
+}
