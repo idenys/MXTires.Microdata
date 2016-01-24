7faa466a9eda3965eb45afe1154a0ab4c4aee0d8
Ivan Denyssevych <idenys@yahoo.com>
2016-01-24 01:04:39 -0800

    Demand in progress

--- Intangible/BusTrip.cs
+++ Intangible/BusTrip.cs
@@ -28,6 +28,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MXTires.Microdata.Places.CivicStructures;
+using MXTires.Microdata.Validators;
 using Newtonsoft.Json;
 
 namespace MXTires.Microdata.Intangible
@@ -37,5 +39,83 @@ namespace MXTires.Microdata.Intangible
     /// </summary>
     public class BusTrip : Thing
     {
+        private Thing arrivalBusStop;
+        /// <summary>
+        /// BusStation  or BusStop - The stop or station from which the bus arrives.
+        /// </summary>
+        /// <value>The produces.</value>
+        [JsonProperty("arrivalBusStop")]
+        public Thing ArrivalBusStop
+        {
+            get { return this.arrivalBusStop; }
+            set
+            {
+                TypeValidator validator = new TypeValidator(typeof(BusStation), typeof(BusStop));
+                validator.Validate(value);
+                this.arrivalBusStop = value;
+            }
+        }
+
+        /// <summary>
+        /// DateTime -The expected arrival time.
+        /// </summary>
+        /// <value>The produces.</value>
+        [JsonProperty("arrivalTime")]
+        public DateTime? ArrivalTime { get; set; }
+
+        /// <summary>
+        /// Text - The name of the bus (e.g. Bolt Express).
+        /// </summary>
+        /// <value>The produces.</value>
+        [JsonProperty("busName")]
+        public String BusName { get; set; }
+
+        /// <summary>
+        /// Text - The unique identifier for the bus.
+        /// </summary>
+        /// <value>The produces.</value>
+        [JsonProperty("busNumber")]
+        public String BusNumber { get; set; }
+
+        private Thing departureBusStop;
+        /// <summary>
+        /// BusStation  or BusStop - The stop or station from which the bus departs.
+        /// </summary>
+        /// <value>The produces.</value>
+        [JsonProperty("departureBusStop")]
+        public Thing DepartureBusStop
+        {
+            get { return this.departureBusStop; }
+            set
+            {
+                TypeValidator validator = new TypeValidator(typeof(BusStation), typeof(BusStop));
+                validator.Validate(value);
+                this.departureBusStop = value;
+            }
+        }
+
+        /// <summary>
+        /// DateTime - The expected departure time.
+        /// </summary>
+        /// <value>The produces.</value>
+        [JsonProperty("departureTime")]
+        public DateTime? DepartureTime { get; set; }
+
+        private Thing provider;
+        /// <summary>
+        /// Person or Organization - The service provider, service operator, or service performer; the goods producer. Another party (a seller) may offer those services or goods on behalf of the provider. A provider may also serve as the seller. Supersedes carrier.
+        /// </summary>
+        /// <value>The produces.</value>
+        [JsonProperty("provider")]
+        public Thing Provider
+        {
+            get { return this.provider; }
+            set
+            {
+                TypeValidator validator = new TypeValidator(typeof(Organization), typeof(ProgramMembership));
+                validator.Validate(value);
+                this.provider = value;
+            }
+        }
     }
 }
--- Intangible/Demand.cs
+++ Intangible/Demand.cs
@@ -23,6 +23,9 @@
 // OTHER DEALINGS IN THE SOFTWARE.
 #endregion
 
+using System;
+using MXTires.Microdata.Intangible.Enumeration;
+using Newtonsoft.Json;
 namespace MXTires.Microdata.Intangible
 {
     /// <summary>
@@ -32,5 +35,165 @@ namespace MXTires.Microdata.Intangible
     /// </summary>
     public class Demand : Thing
     {
+        /// <summary>
+        /// PaymentMethod 	The payment method(s) accepted by seller for this offer.
+        /// </summary>
+        [JsonProperty("acceptedPaymentMethod")]
+        public PaymentMethod AcceptedPaymentMethod { get; set; }
+
+        /// <summary>
+        /// QuantitativeValue 	The amount of time that is required between accepting the offer and the actual usage of the resource or service.
+        /// </summary>
+        [JsonProperty("advanceBookingRequirement")]
+        public QuantitativeValue AdvanceBookingRequirement { get; set; }
+
+        /// <summary>
+        /// Text  or GeoShape  or AdministrativeArea  or Place 	The geographic area where a service or offered item is provided. Supersedes serviceArea.
+        /// </summary>
+        [JsonProperty("areaServed")]
+        public Thing AreaServed { get; set; }
+
+        /// <summary>
+        /// ItemAvailability 	The availability of this item—for example In stock, Out of stock, Pre-order, etc.
+        /// </summary>
+        [JsonProperty("availability")]
+        public ItemAvailability Availability { get; set; }
+
+        /// <summary>
+        /// DateTime 	The end of the availability of the product or service included in the offer.
+        /// </summary>
+        [JsonProperty("availabilityEnds")]
+        public DateTime? AvailabilityEnds { get; set; }
+
+        /// <summary>
+        /// 	DateTime 	The beginning of the availability of the product or service included in the offer.
+        /// </summary>
+        [JsonProperty("availabilityStarts")]
+        public DateTime? AvailabilityStarts { get; set; }
+
+        /// <summary>
+        /// Place 	The place(s) from which the offer can be obtained (e.g. store locations).
+        /// </summary>
+        [JsonProperty("availableAtOrFrom")]
+        public Place AvailableAtOrFrom { get; set; }
+
+        /// <summary>
+        /// DeliveryMethod 	The delivery method(s) available for this offer.
+        /// </summary>
+        [JsonProperty("availableDeliveryMethod")]
+        public DeliveryMethod AvailableDeliveryMethod { get; set; }
+
+        /// <summary>
+        /// BusinessFunction 	The business function (e.g. sell, lease, repair, dispose) of the offer or component of a bundle (TypeAndQuantityNode). The default is http://purl.org/goodrelations/v1#Sell.
+        /// </summary>
+        [JsonProperty("businessFunction")]
+        public BusinessFunction BusinessFunction { get; set; }
+
+        /// <summary>
+        /// QuantitativeValue 	The typical delay between the receipt of the order and the goods leaving the warehouse.
+        /// </summary>
+        [JsonProperty("deliveryLeadTime")]
+        public QuantitativeValue DeliveryLeadTime { get; set; }
+
+        /// <summary>
+        /// BusinessEntityType - The type(s) of customers for which the given offer is valid.
+        /// </summary>
+        [JsonProperty("eligibleCustomerType")]
+        public BusinessEntityType EligibleCustomerType { get; set; }
+       
+        ///QuantitativeValue - The interval and unit of measurement of ordering quantities for which the offer or price specification is valid. This allows e.g. specifying that a certain freight charge is valid only for a certain quantity.
+        [JsonProperty("eligibleQuantity")]
+        public QuantitativeValue EligibleQuantity { get; set; }
+
+        /// <summary>
+        /// QuantitativeValue - The duration for which the given offer is valid. 
+        /// </summary>
+        [JsonProperty("eligibleDuration")]
+        public QuantitativeValue EligibleDuration { get; set; }
+
+        /// <summary>
+        /// PriceSpecification - The transaction volume, in a monetary unit, for which the offer or price specification is valid, e.g. for indicating a minimal purchasing volume, to express free shipping above a certain order volume, or to limit the acceptance of credit cards to purchases to a certain minimal amount.
+        /// </summary>
+        [JsonProperty("eligibleTransactionVolume")]
+        public PriceSpecification EligibleTransactionVolume { get; set; }
+
+        /// <summary>
+        /// Text - The GTIN-12 code of the product, or the product to which the offer refers. The GTIN-12 is the 12-digit GS1 Identification Key composed of a U.P.C. Company Prefix, Item Reference, and Check Digit used to identify trade items. See GS1 GTIN Summary for more details.
+        /// </summary>
+        [JsonProperty("gtin12")]
+        public String Gtin12 { get; set; }
+	
+        /// <summary>
+        /// Text - The GTIN-14 code of the product, or the product to which the offer refers. See GS1 GTIN Summary for more details.
+        /// </summary>
+        [JsonProperty("gtin14")]
+        public String Gtin14 { get; set; }
+	
+        /// <summary>
+        /// Text - The GTIN-8 code of the product, or the product to which the offer refers. This code is also known as EAN/UCC-8 or 8-digit EAN. See GS1 GTIN Summary for more details.
+        /// </summary>
+        [JsonProperty("gtin8")]
+        public String Gtin8 { get; set; }
+
+        /// <summary>
+        /// TypeAndQuantityNode - This links to a node or nodes indicating the exact quantity of the products included in the offer.
+        /// </summary>
+        [JsonProperty("includesObject")]
+        public Thing IncludesObject { get; set; }
+
+        /// <summary>
+        /// Text  or GeoShape  or Place - The ISO 3166-1 (ISO 3166-1 alpha-2) or ISO 3166-2 code, the place, or the GeoShape for the geo-political region(s) for which the offer or delivery charge specification is not valid, e.g. a region where the transaction is not allowed. See also eligibleRegion.
+        /// </summary>
+        [JsonProperty("ineligibleRegion")]
+        public Thing IneligibleRegion { get; set; }
+
+        /// <summary>
+        /// QuantitativeValue - The current approximate inventory level for the item or items.
+        /// </summary>
+        [JsonProperty("inventoryLevel")]
+        public QuantitativeValue InventoryLevel { get; set; }
+
+        /// <summary>
+        /// OfferItemCondition - A predefined value from OfferItemCondition or a textual description of the condition of the product or service, or the products or services included in the offer.
+        /// </summary>
+        [JsonProperty("itemCondition")]
+        public OfferItemCondition ItemCondition { get; set; }
+
+        /// <summary>
+        /// Product  or Service - The item being offered.
+        /// </summary>
+        [JsonProperty("itemOffered")]
+        public Thing ItemOffered { get; set; }
+
+        /// <summary>
+        /// Text 	The Manufacturer Part Number (MPN) of the product, or the product to which the offer refers.
+        /// </summary>
+        [JsonProperty("mpn")]
+        public Thing Mpn { get; set; }
+
+        /// <summary>
+        /// PriceSpecification 	One or more detailed price specifications, indicating the unit price and delivery or payment charges.
+        /// seller	Person  or Text 	The serial number or any alphanumeric identifier of a particular product. When attached to an offer, it is a shortcut for the serial number of the product included in the offer.
+        /// </summary>
+        [JsonProperty("priceSpecification")]
+        public Thing PriceSpecification { get; set; }
+
+        /// <summary>
+        ///Text - The Stock Keeping Unit (SKU), i.e. a merchant-specific identifier for a product or service, or the product to which the offer refers.
+        /// </summary>
+        [JsonProperty("sku")]
+        public String Sku { get; set; }
+
+        /// <summary>
+        /// DateTime 	The date when the item becomes valid.
+        /// </summary>
+        [JsonProperty("validFrom")]
+        public DateTime? ValidFrom { get; set; }
+
+        /// <summary>
+        /// WarrantyPromise - The warranty promise(s) included in the offer. Supersedes warrantyPromise.
+        /// </summary>
+        [JsonProperty("warranty")]
+        public Thing Warranty { get; set; }
     }
 }
