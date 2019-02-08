5ee9594fc872262349478cae2e7438c16736804c
Ivan Denyssevych <idenys@yahoo.com>
2019-02-07 17:57:32 -0800

    Digital document

--- CreativeWorks/DigitalDocument.cs
+++ CreativeWorks/DigitalDocument.cs
@@ -23,34 +23,21 @@
 // OTHER DEALINGS IN THE SOFTWARE.
 #endregion
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MXTires.Microdata.Validators;
+using MXTires.Microdata.Intangible;
 using Newtonsoft.Json;
 
 namespace MXTires.Microdata.CreativeWorks
 {
     /// <summary>
-    /// A strategy of regulating the intake of food to achieve or maintain a specific health-related goal.
+    /// An electronic file or document.
     /// </summary>
-    public class Diet : CreativeWork
+    public class DigitalDocument : CreativeWork
     {
         /// <summary>
-        /// Nutritional information specific to the dietary plan. May include dietary recommendations on what foods to avoid, what foods to consume, and specific alterations/deviations from the USDA or other regulatory body's approved dietary guidelines.
+        /// DigitalDocumentPermission - A permission related to the access to this document (e.g.permission to read or write an electronic document). 
+        /// For a public document, specify a grantee with an Audience with audienceType equal to "public".
         /// </summary>
-        [JsonProperty("dietFeatures")]
-        public string DietFeatures {get;set;}
-
-	
-//endorsers	Person  or 
-//Organization 	People or organizations that endorse the plan.
-//expertConsiderations	Text 	Medical expert advice related to the plan.
-//overview	Text 	Descriptive information establishing the overarching theory/philosophy of the plan. May include the rationale for the name, the population where the plan first came to prominence, etc.
-//physiologicalBenefits	Text 	Specific physiologic benefits associated to the plan.
-//proprietaryName	Text 	Proprietary name given to the diet plan, typically by its originator or creator.
-//risks	Text 	Specific physiologic risks associated to the plan.
+        [JsonProperty("hasDigitalDocumentPermission")]
+        public DigitalDocumentPermission HasDigitalDocumentPermission { get; set; } 
     }
 }
---
+++ Intangible/DigitalDocumentPermission.cs
@@ -0,0 +1,73 @@
+﻿#region License
+// Copyright (c) 2019 1010Tires.com
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+#endregion
+
+#region License
+// Copyright (c) 2019 1010Tires.com
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+#endregion
+
+using MXTires.Microdata.Intangible.Enumeration;
+using Newtonsoft.Json;
+
+namespace MXTires.Microdata.Intangible
+{
+    /// <summary>
+    /// A permission for a particular person or group to access a particular file.
+    /// </summary>
+    public class DigitalDocumentPermission : Thing
+    {
+        /// <summary>
+        /// Audience  or ContactPoint or Organization or Person The person, organization, contact point, or audience that has been granted this permission.
+        /// </summary>
+        [JsonProperty("grantee")]
+        public object Grantee { get; set; }
+
+        /// <summary>
+        /// DigitalDocumentPermissionType   The type of permission granted the person, organization, or audience.
+        /// </summary>
+        [JsonProperty("permissionType")]
+        public DigitalDocumentPermissionType PermissionType { get; set; }
+    }
+}
